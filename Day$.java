45afb71e2f891748ea63183d1861d335b476cdc7
Nikhil <shujian07@gmail.com>
2025-08-18 23:12:45 +0530

    Create Day$.java

---
+++ Day$.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class MergeSortedArrays {
+    static void merge(int[] arr1, int[] arr2, int m, int n) {
+        int i = m - 1, j = 0;
+        while (i >= 0 && j < n) {
+            if (arr1[i] > arr2[j]) {
+                int temp = arr1[i];
+                arr1[i] = arr2[j];
+                arr2[j] = temp;
+            }
+            i--;
+            j++;
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2, 4, 6, 8};
+        merge(arr1, arr2, arr1.length, arr2.length);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+    }
+}
